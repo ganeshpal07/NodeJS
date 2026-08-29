1c9ab57f9abfd0c3e66032580ed1ebd536d35a4b
ganeshpal07 <ganeshpal1516@gmail.com>
2026-08-29 19:43:05 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// console.log("crack google");
+
+const math = require("./math");
+//  this line is used to import functions, objects, variables and other from current directories...
+
+
+console.log(math(2, 5));
---
+++ math.js
@@ -0,0 +1,6 @@
+function add(a, b) {
+    return a+b;
+}
+
+module.exports = add;
+//  this line is used to export functions to other files/directories
